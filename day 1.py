255a09440e799ddf9ea45481cf0d7d15ca4d8ae5
Amir <palsino85@gmail.com>
2023-08-18 14:34:48 +0330

    Upload day 1 and day 2

---
+++ day 1.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator.")
+name_of_city = input("What's name of city you grew up in?\n")
+name_of_pet = input("What's your pet's name?\n")
+print("Your band name coulde be " + name_of_city + " " + name_of_pet)
+
+
---
+++ day 2.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the totla bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
+pay_per_person = round((((tip * total_bill) / 100) + total_bill) / people, 2)
+print(f"Each person should pay : ${pay_per_person}")
